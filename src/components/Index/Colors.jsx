05ddf5cc02f13a2a2a0212390f141ebfa41f609b
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-23 01:23:28 -0500

    resuelto colors

--- src/components/Index/Colors.jsx
+++ src/components/Index/Colors.jsx
@@ -7,61 +7,61 @@ const Colors = () => {
       <div className="wrapper">
         <div className="inner" style={{ "--quantity": 10 }}>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 0, "--color-card": "142, 249, 252" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 1, "--color-card": "142, 252, 204" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 2, "--color-card": "142, 252, 157" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 3, "--color-card": "215, 252, 142" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 4, "--color-card": "252, 252, 142" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 5, "--color-card": "252, 208, 142" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 6, "--color-card": "252, 142, 142" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 7, "--color-card": "252, 142, 239" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 8, "--color-card": "204, 142, 252" }}
           >
             <div className="img"></div>
           </div>
           <div
-            className="card"
+            className="cards"
             style={{ "--index": 9, "--color-card": "142, 202, 252" }}
           >
             <div className="img"></div>
