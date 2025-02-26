391c0ea43e4615488942b8bc919c691696a8a907
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-26 00:40:43 -0500

    avance 2

--- src/components/Products/Productos.jsx
+++ src/components/Products/Productos.jsx
@@ -25,22 +25,26 @@ function Productos() {
     <div className=" place-items-center">
       <div className="mt-28">
         <p className="text-4xl font-bold text-center">PRODUCTOS</p>
-        <div class="border-t-2 border-gray-800 my-5 lg:w-[100vh] w-[35vh]"></div>
+        <div class="border-t-2 border-gray-800 my-5 md:w-[100vh] w-[35vh]"></div>
       </div>
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 place-items-center m-16">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-12 place-items-center m-16">
         {product.map((product) => (
-          <div className="relative group cursor-pointer overflow-hidden duration-500 w-64 h-80 bg-zinc-800 text-gray-50 p-5">
-            <div key={product.id}>
-              <p>{product.name}</p>
-              <div className="absolute w-56 left-0 p-5 -bottom-16 duration-500 group-hover:-translate-y-12">
-                <div className="absolute -z-10 left-0 w-64 h-28 opacity-0 duration-500 group-hover:opacity-50 group-hover:bg-[#64748b]"></div>
+          <div
+          className="relative group cursor-pointer overflow-hidden duration-500 w-80 h-44 bg-zinc-700 text-gray-50 p-5"
+          style={{ borderRadius: "25px" }}
+        >
+          <div key={product.id}>
+            <div className="absolute w-full left-0 p-3 bottom-0 duration-500 group-hover:-translate-y-12">
+              <p className="text-2xl font-normal text-center">{product.name}</p>
+              <div className="mt-0"> 
                 <span className="text-xl font-bold">Hover me!</span>
-                <p className="group-hover:opacity-100 w-56 duration-500 opacity-0">
+                <p className="group-hover:opacity-100  duration-500 opacity-0">
                   {product.info}
                 </p>
               </div>
             </div>
           </div>
+        </div>
         ))}
       </div>
     </div>
