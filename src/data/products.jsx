586c54db01ebfca289181f2a00ee6e5c1dc397ad
AramisSolan0 <gabrielsolanocorporacion20@gmail.com>
2025-03-18 10:05:13 -0500

    fichatecnica

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,15 +1,69 @@
 export const categories = [
     {
       id: 1,
-      name: "categoria 1",
+      name: "LINEA INDUSTRIAL ",
       info: "Productos que acompañan en el resultado",
       img: "/ProductsCard/img1.webp",
       gradient: "linear-gradient(to bottom, rgba(255, 0, 0, 0.30), rgba(255, 0, 0, 0.45))",
       items: [
         {
           id: 1,
-          name: "Autonivellante Epoxico 12KG",
+          name: "Acondicionador de Metales 12KG",
           img: "/carouselproducts/product1.webp",
+          pdf: "/pdf/FT_ACONDICIONADOR_DE_METALES_NEPTUNO.pdf", // Ruta del PDF correspondiente
+            ACABADO: "Mate",
+            RENDIMIENTO: "Variable",
+            PINTADO: "Interior y Exterior",
+            PRESENTACION: "Lt., Galón, 05 Galones , Cilindro",
+            APLICACION: "Superficies metálicascacion",
+        },
+        {
+          id: 2,
+          name: "Protector de Metales",
+          img: "/carouselproducts/product2.webp",
+          pdf: "/pdf/FT_PROTECTOR_DE_METALES_NEPTUNO.pdf", // Ruta del PDF correspondiente
+            ACABADO: "Mate",
+            RENDIMIENTO: "30 m2",
+            PINTADO: "Interior y Exterior",
+            PRESENTACION: "Galónl, 05 Galones, Cilindro",
+            APLICACION: "Brocha o rodillo",
+        },
+        {
+          id: 3,
+          name: "Removedor de Oxido",
+          img: "/carouselproducts/product3.webp",
+          pdf: "/pdf/FT_REMOVEDOR_DE_OXIDO_NEPTUNO.pdf", // Ruta del PDF correspondiente
+            ACABADO: "Mate",
+            RENDIMIENTO: "30 m2",
+            PINTADO: "Interior y Exterior",
+            PRESENTACION: "Lt., Galón, 05 Galones , Cilindro",
+            APLICACION: " inmersión o por frotación",
+        },
+        {
+          id: 4,
+          name: "Transformador de Oxido",
+          img: "/carouselproducts/product4.webp",
+          pdf: "/pdf/FT_TRANSFORMADOR_DE_OXIDO_NEPTUNO.pdf", // Ruta del PDF correspondiente
+            ACABADO: "Mate",
+            RENDIMIENTO: " 1 Lt ",
+            PINTADO: "Interior y Exterior",
+            PRESENTACION: "Lt., Galón, 05 Galones , Cilindro",
+            APLICACION: "Superficies",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "LINEA CONSTRUCCION",
+      info: "Productos que acompañan en el resultado",
+      img: "/ProductsCard/img2.webp",
+      gradient: "linear-gradient(to bottom, rgba(0, 115, 245, 0.45), rgba(0, 115, 245, 0.45))",
+      items: [
+        {
+          id: 11,
+          name: "Removedor de Cemento",
+          img: "/carouselproducts/product1.webp",
+          pdf: "/pdf/FT_REMOVEDOR_DE_CEMENTO_NEPTUNO.pdf", // Ruta del PDF correspondiente
             ACABADO: "Mate",
             RENDIMIENTO: "rendimiento",
             PINTADO: "pintado",
@@ -17,8 +71,19 @@ export const categories = [
             APLICACION: "aplicacion",
         },
         {
-          id: 2,
-          name: "categoria 2",
+          id: 12,
+          name: "Removedor de Salitre",
+          img: "/carouselproducts/product2.webp",
+          pdf: "/pdf/FT_REMOVEDOR_DE_SALITRE_NEPTUNOs.pdf", // Ruta del PDF correspondiente
+            ACABADO: "Mate",
+            RENDIMIENTO: "rendimiento",
+            PINTADO: "pintado",
+            PRESENTACION: "presentacion",
+            APLICACION: "aplicacion",
+        },
+        {
+          id: 13,
+          name: "Bloqueador de Salitre",
           img: "/carouselproducts/product2.webp",
             ACABADO: "Mate",
             RENDIMIENTO: "rendimiento",
@@ -27,9 +92,10 @@ export const categories = [
             APLICACION: "aplicacion",
         },
         {
-          id: 3,
-          name: "Set de pigmentos Metálicos 10 colores x 35G",
-          img: "/carouselproducts/product3.webp",
+          id: 14,
+          name: "Primer Acrílico",
+          img: "/carouselproducts/product2.webp",
+          pdf: "/pdf/FT_PRIMER_ACRÍLICO_NEPTUNO.pdf", // Ruta del PDF correspondiente
             ACABADO: "Mate",
             RENDIMIENTO: "rendimiento",
             PINTADO: "pintado",
@@ -37,28 +103,10 @@ export const categories = [
             APLICACION: "aplicacion",
         },
         {
-          id: 4,
-          name: "Kit de pintura epoxica 1GL",
-          img: "/carouselproducts/product4.webp",
-            ACABADO: "Mate",
-            RENDIMIENTO: "rendimiento",
-            PINTADO: "pintado",
-            PRESENTACION: "presentacion",
-            APLICACION: "aplicacion",
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "Insumos",
-      info: "Productos que acompañan en el resultado",
-      img: "/ProductsCard/img2.webp",
-      gradient: "linear-gradient(to bottom, rgba(0, 115, 245, 0.45), rgba(0, 115, 245, 0.45))",
-      items: [
-        {
-          id: 1,
-          name: "Autonivellante Epoxico 12KG",
-          img: "/carouselproducts/product1.webp",
+          id: 15,
+          name: "Sellador de Fraguas",
+          img: "/carouselproducts/product2.webp",
+          pdf: "/pdf/FT_SELLADOR_DE_FRAGUA_NEPTUNO.pdf", // Ruta del PDF correspondiente
             ACABADO: "Mate",
             RENDIMIENTO: "rendimiento",
             PINTADO: "pintado",
@@ -66,8 +114,8 @@ export const categories = [
             APLICACION: "aplicacion",
         },
         {
-          id: 2,
-          name: "Autonivellante Epoxico 12KG",
+          id: 16,
+          name: "Superclean Multiusos",
           img: "/carouselproducts/product2.webp",
             ACABADO: "Mate",
             RENDIMIENTO: "rendimiento",
@@ -85,7 +133,7 @@ export const categories = [
       gradient: "linear-gradient(to bottom, rgba(155, 226, 23, 0.55), rgba(155, 226, 23, 0.55))",
       items: [
         {
-          id: 1,
+          id: 21,
           name: "Autonivellante Epoxico 12KG",
           img: "/carouselproducts/product1.webp",
           ACABADO: "Mate",
@@ -95,7 +143,7 @@ export const categories = [
           APLICACION: "aplicacion",
         },
         {
-          id: 2,
+          id: 22,
           name: "Autonivellante Epoxico 12KG",
           img: "/carouselproducts/product2.webp",
           ACABADO: "Mate",
@@ -114,7 +162,7 @@ export const categories = [
       gradient: "linear-gradient(to bottom, rgba(255, 176, 30, 0.616), rgba(255, 176, 30, 0.678))",
      items: [
         {
-          id: 1,
+          id: 31,
           name: "Autonivellante Epoxico 12KG",
           img: "/carouselproducts/product1.webp",
           ACABADO: "Mate",
@@ -124,7 +172,7 @@ export const categories = [
           APLICACION: "aplicacion",
         },
         {
-          id: 2,
+          id: 32,
           name: "Autonivellante Epoxico 12KG",
           img: "/carouselproducts/product2.webp",
           ACABADO: "Mate",
@@ -135,4 +183,5 @@ export const categories = [
         },
       ],
     },
+    
   ];
