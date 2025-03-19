82b94c5e0ab7ece16d8b5e719a45b93174c3c6a7
AramisSolan0 <gabrielsolanocorporacion20@gmail.com>
2025-03-19 12:46:07 -0500

    casi termiando

--- src/components/Index/Whatsapp.jsx
+++ src/components/Index/Whatsapp.jsx
@@ -5,7 +5,7 @@ const WhatsApp = () => {
   return (
     <div className="fixed bottom-5 right-5 z-50">
       <a
-        href="link del numerito" 
+        href="https://w.app/wpux4n" 
         target="_blank"
         rel="noopener noreferrer"
         className="bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300 w-16 h-16 flex items-center justify-center"
