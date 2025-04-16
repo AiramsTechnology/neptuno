633dff9c843d8af1afab03d3f413265e4c96d717
AiramsTechnology <airamstechnology@gmail.com>
2025-04-16 18:07:07 -0500

    esd

--- src/components/Index/ImgPrincipal.jsx
+++ src/components/Index/ImgPrincipal.jsx
@@ -5,7 +5,7 @@ function ImgPrincipal() {
     <header className="header w-full max-w-screen" id="inicio">
       <div className="w-[100%] overflow-hidden mx-auto h-screen">
         <div className="text-start p-0 h-full flex items-center justify-center text-[#272770]">
-          <h1 className="text-[45px] mb-[15px] font-bold mx-10 md:text-7xl md:font-semibold lg:mr-64 lg:font-semibold">
+          <h1 className="text-[45px] mb-[15px] font-bold mx-10 md:text-7xl md:font-semibold lg:mr-64 lg:font-semibold" translate="no">
             <div className="flex flex-col space-y-0">
               <TextEffect
                 per="char"
